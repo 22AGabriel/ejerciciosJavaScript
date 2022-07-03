e6fe45f7aba0e80c2216dce869a099e3711a2908
Gabriel Azubel <gabrielazubel63t@gmail.com>
2022-07-03 13:43:14 -0300

    realice los primeros 5 ejercicios

---
+++ js/ejercicios1al5.js
@@ -0,0 +1,17 @@
+// Ejercicio 1
+alert("un mensaje");
+
+// Ejercicio 2
+document.write("<h1>Hello World</h1>");
+
+// Ejercicio 3
+document.write("<p>" + (3 + 5) + "</p>");
+
+// Ejercicio 4
+let nombre = prompt("Ingrese su nombre");
+document.write("<p>Hola " + nombre + ".</p>");
+
+// Ejercicio 5
+let valor1 = parseInt(prompt("Ingrese el primer número."));
+let valor2 = parseInt(prompt("ingrese el segundo número."));
+document.write("<p>" + (valor1 + valor2) + "</p>");
