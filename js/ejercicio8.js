3a8ef83eb06a12db4bbedeb837cc0a08f65d2ca2
Gabriel Azubel <gabrielazubel63t@gmail.com>
2022-07-06 20:02:31 -0300

    realicé el ejercicio 9

--- js/ejercicio8.js
+++ js/ejercicio8.js
@@ -1,6 +1,6 @@
 let var1 = parseInt(prompt("Ingrese un valor:"));
 
-if ((var1 % 2) == 0){
+if ((var1 % 2) === 0){
     document.write("El valor ingresado es divisible por 2");
 } else {
     document.write("El valor ingresado no es divisible por 2");   
